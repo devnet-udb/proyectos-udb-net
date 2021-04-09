ecbdbe0d6f2b3090c4acdb68703d8b69349a48da
Carlos Enrique Lemus Serrano <cwlemus@gmail.com>
2021-04-08 23:55:59 -0500

    [08-04-2021:13:34] - Guadar saludo

--- AppNetGit2/Program.cs
+++ AppNetGit2/Program.cs
@@ -7,6 +7,7 @@ namespace AppNetGit2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Saludo desde local");
         }
     }
 }

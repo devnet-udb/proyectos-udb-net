9c91155e6305e9bda5b514f350ddc1b11c8eec8b
Carlos Enrique Lemus Serrano <cwlemus@gmail.com>
2021-04-08 23:28:29 -0500

    [09-04-2021:13:00] - Agregar proyecto inicial

---
+++ AppNetGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppNetGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
